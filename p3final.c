f6016302f6d8888833725abe61bd1a868a1aaa26
Sahana karishetti karishetti <96462274+Sahanakarishetti1@users.noreply.github.com>
2022-03-31 12:54:26 +0000

    p3debug completed

--- p3final.c
+++ p3final.c
@@ -17,19 +17,18 @@ int input()
 
 int isprime(int n)
 {
-  if (n==0 || n==1) {
-    return 0;
-  }
-  if(n==2) {
-    return 1;
-  }
+  
   for(int i= 2; i<= n;i++)
-  {
-    if(n%i==0)
+    {
+    if(n%i==0){
       return 0;
+      }
+    else{
+      return 1;
+    }
   }
-  return 1;
-}
+  }
+ 
 void output(int n, int isp)
 {
   if(isp == 0)
